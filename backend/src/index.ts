77ab4c35533fa7b7bb25fbdbc1afea50441933cf
JayantRautela <jayantrautela0205@gmail.com>
2026-02-15 23:43:52 +0530

    add routes to main file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
+import TaxRouter from "./routes/tax.routes.js";
+import SubsidyRouter from "./routes/subsidy.routes.js";
 
 const app = express();
 
@@ -9,6 +11,9 @@ app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT;
+
+app.use('/api/v1/tax', TaxRouter);
+app.use('/api/v1/subsidy', SubsidyRouter);
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
